4b95d677688aa906edf770e44df3cd9392aaa47c
novebug <zita1122334@gmail.com>
2023-07-11 23:53:32 +0900

    공격제작준비

--- Assets/10303/01.Scripts/Player_Move.cs
+++ Assets/10303/01.Scripts/Player_Move.cs
@@ -8,6 +8,7 @@ public class Player_Move : MonoBehaviour
     private Rigidbody2D _rb;
     [SerializeField]
     private float _speed = 0;
+    private BoxCollider2D _coll;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -23,5 +24,13 @@ public class Player_Move : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         _rb.velocity = new Vector2(x, y) * _speed;
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            
+        }
     }
 }
